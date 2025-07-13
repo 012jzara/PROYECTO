a55a36fcd8e9ca21e32cd28927a7e43b0efcd666
mabel llanos <143773112+012jzara@users.noreply.github.com>
2025-07-13 03:20:41 -0600

    Delete models directory

--- models/Usuario.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UsuarioSchema = new mongoose.Schema({
-    usuario: String,
-    contrasena: String,
-    rol: String
-});
-
-module.exports = mongoose.model('Usuario', UsuarioSchema);
