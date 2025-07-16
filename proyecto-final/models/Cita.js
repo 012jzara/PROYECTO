7566887ed8c1353a9022a26cc3958c29aea49af8
mabel llanos <143773112+012jzara@users.noreply.github.com>
2025-07-16 17:36:42 -0600

    Update Cita.js

--- proyecto-final/models/Cita.js
+++ proyecto-final/models/Cita.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+const historialSchema = new mongoose.Schema({
+    FechaCambio: { type: Date, default: Date.now },
+    EstadoAnterior: String,
+    EstadoNuevo: String,
+    Motivo: String,
+    UsuarioResponsable: String
+}, { _id: false });
+
 const CitaSchema = new mongoose.Schema({
     EsClienteExistente: Boolean,
     NombrePropietario: String,
