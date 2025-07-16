4a74ac48c891a65a304fd2a11c8c059d14a9f5e1
mabel llanos <143773112+012jzara@users.noreply.github.com>
2025-07-16 16:31:11 -0600

    Update historialCita.js

--- proyecto-final/routes/historialCita.js
+++ proyecto-final/routes/historialCita.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerHistorialPorCita } = require('../controllers/historialCitaController');
+const { obtenerHistorialPorCita } = require('../controllers/HistorialCitaController');
 
 router.get('/:citaId', obtenerHistorialPorCita);
 
