be3fda46846b891a94063acb3eac600831750f5e
mabel llanos <143773112+012jzara@users.noreply.github.com>
2025-12-20 15:37:09 -0600

    Remove redundant lines in productoInsumo.js

--- proyecto-final/routes/productoInsumo.js
+++ proyecto-final/routes/productoInsumo.js
@@ -8,13 +8,13 @@ const ROLES_INV = ['Admin', 'Inventario'];
 router.post('/',authenticate,permitirRoles(ROLES_INV),auditar('PRODUCTO_CREAR'),producto.agregarProducto);
 router.get('/',authenticate,permitirRoles(['Admin', 'Inventario', 'Caja', 'Veterinario']),auditar('PRODUCTO_LISTAR'),producto.obtenerProductos);
 router.get('/buscar/:filtro', authenticate,permitirRoles(['Admin', 'Inventario', 'Caja', 'Veterinario']),auditar('PRODUCTO_BUSCAR'),producto.buscarProducto);
+router.get('/filtro/categoria/:categoria', producto.filtrarPorCategoria);
+router.get('/filtro/tienda/:tienda', producto.filtrarPorTienda);
+router.get('/stock/critico', producto.obtenerStockCritico);
 router.put('/movimiento/:id',authenticate,permitirRoles(ROLES_INV),auditar('PRODUCTO_MOVIMIENTO'),producto.movimientoProducto);
 router.get('/:id',authenticate,permitirRoles(['Admin', 'Inventario', 'Caja', 'Veterinario']),auditar('PRODUCTO_POR_ID'),producto.obtenerProductoPorId);
 router.put('/:id', authenticate, permitirRoles(ROLES_INV),auditar('PPRODUCTO_ACTUALIZAR'),producto.actualizarProducto);
 router.delete('/:id', authenticate,permitirRoles(['Admin']),auditar('PRODUCTO_ELIMINAR'),producto.eliminarProducto);
-router.get('/filtro/categoria/:categoria', producto.filtrarPorCategoria);
-router.get('/filtro/tienda/:tienda', producto.filtrarPorTienda);
-router.get('/stock/critico', producto.obtenerStockCritico);
-
 module.exports = router;
+
 
